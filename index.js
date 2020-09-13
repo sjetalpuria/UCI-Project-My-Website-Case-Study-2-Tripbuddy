32129959ab4596ed46985c5219cf64ea3653f59c
sjetalpuria <shivani.jetalpuria@gmail.com>
2020-09-13 11:36:54 -0700

    About Me page updated
    Yay!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//loading
 function fade() {
   $('.preloader').fadeOut("slow");
   }
@@ -38,48 +38,7 @@ var main = function() {
       });
     };
   
-    //About Me
-    window.onload=function()
-    {
-      const ABOUT = document.querySelector(".About_Me");
-
-      window.addEventListener('scroll', scrollEffect);
-
-      function scrollEffect()
-      {
-        if (window.scrollY >= 500)
-        {
-          ABOUT.style.opacity='1';
-          ABOUT.style.transform= 'translateX(0px)';
-          ABOUT.style.transition= '1s ease-in-out';
-        } 
-        
-        else
-        {
-          ABOUT.style.opacity='1';
-          ABOUT.style.transform= 'translateY(-50px)';
-        }
-       
-      }
-      scrollEffect();
-      
-    }
-
-    //work
-    $( ".Grid_Container_Case_Studies" ).click(function() {
-      $( "Grid_Container_Case_Studies" ).scroll();
-    });
-    //Contact Form
-    $('.contact-form').find('.form-control').each(function() {
-      var targetItem = $(this).parent();
-      if ($(this).val()) {
-        $(targetItem).find('label').css({
-          'top': '10px',
-          'fontSize': '14px'
-          
-        });
-      }
-    })
+    //contact        
     $('.Contact').find('.form-control').focus(function() {
       $(this).parent('.input-block').addClass('focus');
       $(this).parent().find('label').animate({
@@ -146,5 +105,5 @@ var main = function() {
      });
   
   
-    //
+
     
